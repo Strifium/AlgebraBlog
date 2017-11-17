85452c0892ab8547a686b6ffa04bc1751fa42425
programeri <programeri@ucione.local>
2017-11-17 21:47:59 +0100

    Add migrations and models

---
+++ app/Models/Comment.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    /**
+	 * The attributes that are mass assignable
+	 *
+	 * @var array
+	 */
+	 protected $fillabe = ['user_id','post_id','content'];
+	 
+	 /**
+	 * The Eloquent users model name
+     *
+	 * @var string
+	 */
+	 protected static $usersModel = 'App\Models\Users';
+	 
+	 /**
+	 * The Eloquent posts model name
+     *
+	 * @var string
+	 */
+	 protected static $postsModel = 'App\Models\Post';
+	 
+	 /**
+	 * Returns the user relationship
+	 *
+	 * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+	 */
+	 
+	 public function user()
+	 {
+		 return $this->belongsTo(static::$usersModel,'user_id');
+	 }
+	 
+	 /**
+	 * Returns the posts relationship
+	 *
+	 * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+	 */
+	 
+	 public function post()
+	 {
+		 return $this->belongsTo(static::$postModel,'post_id');
+	 }
+	 
+	 /**
+	 * Save Comment
+	 *
+	 * @param array $comment
+	 * @return void 
+	 */
+	 public function savePost($comment=array())
+	 {
+		 return $this->fill($comment)->save();
+	 }
+	 
+	 /**
+	 * Update Comment
+	 *
+	 * @param array $comment
+	 * @return void 
+	 */
+	 public function updateComment($comment=array())
+	 {
+		 return $this->update($comment);
+	 }
+	 
+}
